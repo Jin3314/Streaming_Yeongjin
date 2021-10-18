a6191ac408c275c83eedf4724c4101727c5e176f
Jin3314 <dudwlstldud@gmail.com>
2021-10-17 22:33:14 -0400

    Add assignment1(save and load)
    Add assignment1(save and load)

--- StreamingDataLab/Assets/Assignment.cs
+++ StreamingDataLab/Assets/Assignment.cs
@@ -1,4 +1,3 @@
-
 /*
 This RPG data streaming assignment was created by Fernando Restituto.
 Pixel RPG characters created by Sean Browning.
@@ -7,33 +6,25 @@ Pixel RPG characters created by Sean Browning.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 
 #region Assignment Instructions
 
 /*  Hello!  Welcome to your first lab :)
-
 Wax on, wax off.
-
     The development of saving and loading systems shares much in common with that of networked gameplay development.  
     Both involve developing around data which is packaged and passed into (or gotten from) a stream.  
     Thus, prior to attacking the problems of development for networked games, you will strengthen your abilities to develop solutions using the easier to work with HD saving/loading frameworks.
-
     Try to understand not just the framework tools, but also, 
     seek to familiarize yourself with how we are able to break data down, pass it into a stream and then rebuild it from another stream.
-
-
 Lab Part 1
-
     Begin by exploring the UI elements that you are presented with upon hitting play.
     You can roll a new party, view party stats and hit a save and load button, both of which do nothing.
     You are challenged to create the functions that will save and load the party data which is being displayed on screen for you.
-
     Below, a SavePartyButtonPressed and a LoadPartyButtonPressed function are provided for you.
     Both are being called by the internal systems when the respective button is hit.
     You must code the save/load functionality.
     Access to Party Character data is provided via demo usage in the save and load functions.
-
     The PartyCharacter class members are defined as follows.  */
 
 public partial class PartyCharacter
@@ -48,19 +39,14 @@ public partial class PartyCharacter
     public int wisdom;
 
     public LinkedList<int> equipment;
-
+    //public LinkedList<int> affliction;
 }
-
-
 /*
     Access to the on screen party data can be achieved via …..
-
     Once you have loaded party data from the HD, you can have it loaded on screen via …...
-
     These are the stream reader/writer that I want you to use.
     https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter
     https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader
-
     Alright, that’s all you need to get started on the first part of this assignment, here are your functions, good luck and journey well!
 */
 
@@ -72,24 +58,56 @@ public partial class PartyCharacter
 
 static public class AssignmentPart1
 {
-
+    const int Pcharacter = 0;
+    const int EqSaveData = 1;
     static public void SavePartyButtonPressed()
     {
+        // Get the directories currently on the C drive.
+        // DirectoryInfo[] cDirs = new DirectoryInfo(@"C:\Users\User\Downloads\StreamingDataLab-main\StreamingDataLab\Assets").GetDirectories();
+
+        using StreamWriter sw = new StreamWriter(Application.dataPath + Path.DirectorySeparatorChar + "JinData.txt");
+    
         foreach (PartyCharacter pc in GameContent.partyCharacters)
         {
             Debug.Log("PC class id == " + pc.classID);
+            
+            sw.WriteLine(Pcharacter + "," + pc.classID + "," + pc.health + "," + pc.mana + "," + pc.strength + "," + pc.agility + "," + pc.wisdom);
+
+            foreach (int equipID in pc.equipment)
+            {
+                sw.WriteLine(EqSaveData + "," + equipID);
+
+            }
         }
+        sw.Close();
     }
 
     static public void LoadPartyButtonPressed()
     {
 
-        //GameContent.partyCharacters.Clear();
+        GameContent.partyCharacters.Clear();
 
+        StreamReader sr = new StreamReader(Application.dataPath + Path.DirectorySeparatorChar + "JinData.txt");
+        string line;
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            Debug.Log(line);
+            string[] csv = line.Split(','); 
+            Debug.Log(csv[0]); 
+            int saveDataSignifier = int.Parse(csv[0]);
+            if (saveDataSignifier == Pcharacter) 
+            {
+                PartyCharacter pc = new PartyCharacter(int.Parse(csv[1]), int.Parse(csv[2]), int.Parse(csv[3]), int.Parse(csv[4]), int.Parse(csv[5]), int.Parse(csv[6]));
+                GameContent.partyCharacters.AddLast(pc);
+            }
+            else if (saveDataSignifier == EqSaveData)
+            {
+                GameContent.partyCharacters.Last.Value.equipment.AddLast(int.Parse(csv[1]));
+            }
+        }
         GameContent.RefreshUI();
-
     }
-
 }
 
 
@@ -108,23 +126,16 @@ static public class AssignmentConfiguration
 }
 
 /*
-
 In this part of the assignment you are challenged to expand on the functionality that you have already created.  
     You are being challenged to save, load and manage multiple parties.
     You are being challenged to identify each party via a string name (a member of the Party class).
-
 To aid you in this challenge, the UI has been altered.  
-
     The load button has been replaced with a drop down list.  
     When this load party drop down list is changed, LoadPartyDropDownChanged(string selectedName) will be called.  
     When this drop down is created, it will be populated with the return value of GetListOfPartyNames().
-
     GameStart() is called when the program starts.
-
     For quality of life, a new SavePartyButtonPressed() has been provided to you below.
-
     An new/delete button has been added, you will also find below NewPartyButtonPressed() and DeletePartyButtonPressed()
-
 Again, you are being challenged to develop the ability to save and load multiple parties.
     This challenge is different from the previous.
     In the above challenge, what you had to develop was much more directly named.
@@ -135,9 +146,7 @@ Again, you are being challenged to develop the ability to save and load multiple
         What are the things that you will need to do?  
     So much of development is just breaking problems down into smaller parts.
     Take the time to name each part of what you will create and then, do it.
-
 Good luck, journey well.
-
 */
 
 static public class AssignmentPart2
@@ -183,5 +192,3 @@ static public class AssignmentPart2
 }
 
 #endregion
-
-
